af967355e6ae84c78ae0a953a02ff6eba8f6fbe0
StephenJMarden <stephen.j.marden@gmail.com>
2018-10-14 21:48:27 -0400

    heroku troubleshooting 3

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const   app = require('./app'),
         http = require('http'),
-        port = process.env.PORT || 3000;
+        port = process.env.PORT || 4000;
 
 console.log(port);
 app.set('port', port);
