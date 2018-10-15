5336e76f0d4e0ae1999cc323d4028e1ccb71c730
StephenJMarden <stephen.j.marden@gmail.com>
2018-10-14 21:44:49 -0400

    heroku troubleshooting 2

--- src/server.js
+++ src/server.js
@@ -2,10 +2,10 @@ const   app = require('./app'),
         http = require('http'),
         port = process.env.PORT || 3000;
 
+console.log(port);
 app.set('port', port);
 
 const server = http.createServer(app);
-console.log(port);
 server.listen(port);
 
 server.on('listening', () => {
