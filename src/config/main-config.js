d3a2e55ab9f34ef86d0d1748b2a4a81e15727de8
StephenJMarden <stephen.j.marden@gmail.com>
2018-10-18 21:39:44 -0400

    topic-resource-2 checkpoint completed

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const   path = require('path'),
-        viewsFolder = path.join(__dirname, '..', 'views');
+        viewsFolder = path.join(__dirname, '..', 'views'),
+        bodyParser = require('body-parser');
 
 module.exports = {
     init(app, express) {
         app.set('views', viewsFolder);
         app.set('view engine', 'ejs');
+        app.use(bodyParser.urlencoded({extended: true}));
         app.use(express.static(path.join(__dirname, '..', 'assets')));
     }
 }
