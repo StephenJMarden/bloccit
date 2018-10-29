92d2d33396ab0f77230e5ae8b4e3acdc28c69948
StephenJMarden <stephen.j.marden@gmail.com>
2018-10-29 14:26:59 -0400

    post-resource-2 checkpoint completed

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -1,11 +1,13 @@
 const   staticRoutes = require('../routes/static'),
         topicRoutes = require('../routes/topics'),
-        advertisementRoutes = require('../routes/advertisements');
+        advertisementRoutes = require('../routes/advertisements'),
+        postRoutes = require('../routes/posts');
 
 module.exports = {
     init(app) {
         app.use(staticRoutes);
         app.use(topicRoutes);
         app.use(advertisementRoutes);
+        app.use(postRoutes);
     }
 }
